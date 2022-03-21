805c2f597db81e664df683d67618350d124b9ed0
VinCoD <5895.2020@students.ku.ac.ke>
2022-03-21 11:25:48 +0300

    Added Gallery model

--- manuelgallery/models.py
+++ manuelgallery/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Gallery(models.Model):
+    # uploaded_by = models.ForeignKey(Client, on_delete=models.CASCADE)
+    photos = models.ImageField
+    gallery_name = models.CharField(max_length=100)
+    date_added = models.DateTimeField(auto_now_add= True)
+    caption = models.TextField(max_length=200)
+
+    class Meta:
+        verbose_name_plural = "Galleries"
+
+    def __str__(self):
+        return self.gallery_name
